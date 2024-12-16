623d1a72cf3e53cfe07b3165e7a20b95c098251b
Donnie Yang <yanghs.dgut@gmail.com>
2024-12-16 14:23:49 +0800

    add donnieeshop

--- donnieeshop/sanity.config.ts
+++ donnieeshop/sanity.config.ts
@@ -1,16 +1,16 @@
 import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
-  title: 'sanity-project',
+  title: 'donnieeshop',
 
-  projectId: 'dka6j2zq',
+  projectId: 'iiuh03pt',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
